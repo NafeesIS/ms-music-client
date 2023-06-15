098c46265266c5d212516e2eae1b780e65233b4a
NafeesIS <nafeesshwaon555@gmail.com>
2023-06-15 19:43:42 +0600

    another commit

--- src/Hooks/PrivateRoute/PrivateRoute.jsx
+++ src/Hooks/PrivateRoute/PrivateRoute.jsx
@@ -1,34 +1,25 @@
-import { useContext, useEffect, useState } from "react";
-// import { AuthContext } from "../Provider/AuthProvider";
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { RingLoader } from "react-spinners";
 import { AuthContext } from "../Provider/AuthProvider/AuthProvider";
+import { RingLoader } from "react-spinners";
+
 const PrivateRoute = ({ children }) => {
-    const { user } = useContext(AuthContext);
-    const [isLoading, setIsLoading] = useState(true);
+    const { user, loading } = useContext(AuthContext);
 
-    useEffect(() => {
-        // Simulating async loading of user authentication state
-        const checkAuthState = () => {
-            // You can replace the setTimeout with your actual logic to check the user's authentication state
-            setTimeout(() => {
-                setIsLoading(false);
-            }, 1000);
-        };
-
-        checkAuthState();
-    }, []);
-
-    if (isLoading) {
+    if (loading) {
         // Show a loading indicator or skeleton screen while checking the authentication state
-        return <div className="flex items-center justify-center h-screen"><RingLoader color="#36d7b7" /></div>;
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <RingLoader color="#36d7b7" />
+            </div>
+        );
     }
 
     if (user) {
         return children;
     }
 
-    return <Navigate to={'/login'} replace={true} />;
+    return <Navigate to={"/login"} replace={true} />;
 };
 
 export default PrivateRoute;
